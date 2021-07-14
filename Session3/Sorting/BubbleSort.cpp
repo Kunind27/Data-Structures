ac0ebafba321b8a0b34e59772e81cc750efda5f1
Kunind Sahu <52349865+Kunind27@users.noreply.github.com>
2021-07-15 00:08:06 +0530

    Added code for Bubble Sort

--- Session3/Sorting/BubbleSort.cpp
+++ Session3/Sorting/BubbleSort.cpp
@@ -1,1 +1,48 @@
+#include <iostream>
+using namespace std;
+// Implementing Bubble Sort 
 
+void bubbleSort(int a[], int len)
+{
+    // Looping only until the second last element
+    for(int i=0; i<len-1; i++)
+    {
+        bool swapped = false;
+        for(int j=0; j<len-1-i; j++)
+        {
+            // Swapping the elements if they are in the wrong order
+            if(a[j]>a[j+1])
+            {
+                // Invariant is that the last elements are the largest in the array and get fixed.
+                int temp = a[j];
+                a[j] = a[j+1];
+                a[j+1] = temp;
+                swapped = true;
+            }
+        }
+        // Automatically break the loop early if there have been no swaps - that means loop is sorted
+        if(swapped==false)
+            break;
+    }
+}
+
+int main()
+{
+    int a[] = {4,6,2,1,3,5,10,9,14,73,67,22};
+    int len = sizeof(a)/sizeof(a[0]);
+    
+    // printing original array 
+    cout<<"Original Array"<<endl;
+    for(int i=0; i<len; i++)
+        cout<<a[i]<<" ";
+    cout<<endl;
+    
+    bubbleSort(a, len);
+    
+    // printing sorted array 
+    cout<<"Sorted Array"<<endl;
+    for(int i=0; i<len; i++)
+        cout<<a[i]<<" ";
+	
+	return 0;
+}
